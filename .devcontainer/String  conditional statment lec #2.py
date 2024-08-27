a65beeb6d0f1698e76a72af5cdc64b4c4f6bb7fc
Faisal-zia <engr.faisalzia@gmail.com>
2024-08-27 19:14:42 +0000

    string-conditions-in-python

---
+++ .devcontainer/String  conditional statment lec #2.py
@@ -0,0 +1,37 @@
+# Escape sequence chrachters \n and \t
+str1 = "this is Faisal-zia \n i have done my textile engineerign."
+print(str1)
+
+str2 = "i m going to lahore \t we will attend class togather"
+print(str2)
+
+
+# different operation
+# 1 # concatenation (adding two words)
+
+str3 = "engr-Faisal"
+str4 = "zia"
+print(str3+str4)
+
+# 2 # length of function
+
+str5 = "faisal-zia+" "+sheikh"
+len1 = len(str5)
+print(len1)
+
+# Indexing 
+
+str6 = "Faisal-zia"
+print(str6[2])
+
+# Slicing (broke string into pieces)
+
+# str[starting-index:ending-index] ending-index not included
+str7 = "Faisal-zia"
+
+print(str7[2:6])  # isal
+ # negative index
+ 
+str8 = "Mohid-zia"
+print(str8[-6:-2]) # id-z
+
---
+++ .devcontainer/string-functions.py
@@ -0,0 +1,47 @@
+# different string functions
+# endswith("AIC.")
+
+str = "i am faisal zia, i am learning python from PIAIC."
+print(str.endswith("AIC."))
+
+# capitalize
+
+str1 = "i am faisal zia, i am learning python from PIAIC."
+print(str1.capitalize())
+
+# Replaced old,new
+
+str2 = "i am faisal zia, i am learning python from PIAIC."
+print(str2.replace("a","o"))
+print(str2.replace("faisal","mohid"))
+
+# find number or word
+
+str3 = "i am faisal zia, i am learning python from PIAIC."
+print(str3.find("r"))
+print(str3.find("faisal"))
+
+str4 = "i am faisal zia, i am learning python from PIAIC."
+print(str4.count("faisal"))
+
+# practice 
+
+name=input("enter your name: ")
+print("length of your name is", len(name))
+
+# conditional statments
+
+# if
+age=24 
+if(age >= 18):
+  print("can vote")
+  print("can drive")
+
+# elif
+light = "green"
+if(light == "red"):
+    print("stop")
+elif(light == "green"):
+    print("go")
+    
+# else
